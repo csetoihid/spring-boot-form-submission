95c8a6f8b13ebed7a76135ba511b56591a689a66
csetoihid <cse.toihid@gmail.com>
2019-04-26 00:36:40 +0200

    upload

---
+++ src/main/java/operation/StringOperationApplication.java
@@ -0,0 +1,13 @@
+package operation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringOperationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/operation/StringOperationApplicationTests.java
@@ -0,0 +1,16 @@
+package operation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StringOperationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
